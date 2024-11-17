2f22e099895fabebb61dfcfc2bd382abff5aa36e
howetuft <howetuft@gmail.com>
2024-11-17 20:14:08 +0100

    fix pyluxcoremaketx

--- src/pyluxcoremaketx.py
+++ src/pyluxcoremaketx.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import sys
-import pyluxcoretools.pyluxcoreconsole.cmd
+import pyluxcoretools.pyluxcoremaketx.cmd
 
 def main():
     argv = sys.argv
-    pyluxcoretools.pyluxcoreconsole.cmd.main(argv)
+    pyluxcoretools.pyluxcoremaketx.cmd.main(argv)
